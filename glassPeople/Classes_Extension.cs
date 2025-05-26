46f6a3b893c315579c43311d61c8da20f0ee53ef
SergSuhanov <sergsuhanov@gmail.com>
2025-05-26 07:59:28 +0300

    fix

---
+++ glassPeople/Classes_Extension.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Collections.Generic;
+
+namespace glassPeople.ActivityLibrary.glassCAD.Planing.Model {
+    public partial class AppointmentItem {
+        public IEnumerable<AppointmentItem> GetChilds(PlanningContext pc = null) => null;
+        public IEnumerable<AppointmentItem> GetAllChilds(PlanningContext pc = null) => null;
+
+        public IEnumerable<AppointmentItem> GetAllParents(PlanningContext pc = null) => null;
+        public IEnumerable<AppointmentItem> GetParents(PlanningContext pc = null) => null;
+
+        public IEnumerable<AppointmentItem> GetAll() => null;
+    }
+}
